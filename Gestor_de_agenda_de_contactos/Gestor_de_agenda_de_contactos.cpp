80d7643cd48e14f3776ca091c50ea9ac0f329cf9
jimenaaguero2-design <jimenaaguero2@gmail.com>
2025-12-07 13:35:35 -0600

    Avance_#3
    Se implementaron los métodos de búsqueda solicitados, en este caso el secuencial y la binaria con el nombre, adicional a esto, se cambiaron detalles de diseño, con el fin de que se vea más limpio y ordenado el código

--- Gestor_de_agenda_de_contactos/Gestor_de_agenda_de_contactos.cpp
+++ Gestor_de_agenda_de_contactos/Gestor_de_agenda_de_contactos.cpp
@@ -17,7 +17,7 @@ int capacidad = TAM;
 int cantidad = 0;
 
 // Función para crecer el arreglo
-void crecerArreglo() {
+void CrecerArreglo() {
     int nuevaCapacidad = capacidad * 2;
     Contacto* nuevo = new Contacto[nuevaCapacidad];
 
@@ -31,9 +31,9 @@ void crecerArreglo() {
 }
 
 // Función para agregar
-void agregarContacto() {
+void AgregarContacto() {
     if (cantidad == capacidad) {
-        crecerArreglo();   // Si se llena, crece
+        CrecerArreglo();   // Si se llena, crece
     }
 
     cout << "Digite un nombre: ";
@@ -47,7 +47,7 @@ void agregarContacto() {
 }
 
 // Función para mostrar
-void mostrarContactos() {
+void MostrarContactos() {
     cout << "\n----CONTACTOS REGISTRADOS----\n";
     for (int i = 0; i < cantidad; i++) {
         cout << arreglo[i].nombre << endl;
@@ -57,7 +57,7 @@ void mostrarContactos() {
     }
 }
 
-void ordenarPorNombre() { // Función para ordenar los nombres de manera alfabetica 
+void OrdenarPorNombre() { // Función para ordenar los nombres de manera alfabetica 
 
     if (cantidad < 2) { cout << "\nNo hay suficientes contactos para ordenar...\n"; return; }
     for (int i = 0; i < cantidad - 1; i++) {
@@ -72,35 +72,84 @@ void ordenarPorNombre() { // Función para ordenar los nombres de manera alfabet
     cout << "\nSe ordenaron los contactos alfabeticamente...\n";
 }
 
+int BusquedaSecuencialNombre(string nombre) {  // Función para buscar de manera secuencial
+    for (int i = 0; i < cantidad; i++) {
+        if (arreglo[i].nombre == nombre) return i;
+    }
+    return -1;
+}
+
+int BusquedaBinariaNombre(string nombre) { // Función para buscar de manera binaria, pero debe estar ordenados los nombres
+    int ini = 0, fin = cantidad - 1;
+    while (ini <= fin) {
+        int mid = (ini + fin) / 2;
+        if (arreglo[mid].nombre == nombre) return mid;
+        else if (nombre < arreglo[mid].nombre) fin = mid - 1;
+        else ini = mid + 1;
+    }
+    return -1;
+}
+
+
 int main() {
     arreglo = new Contacto[capacidad];  // Creamos el arreglo dinámico
 
     int op;
+    int secu;
+    int bina;
+    string nombre;
     do {
-        cout << "\n------MENU-----\n";
+        cout << "================ MENU ====================\n";
         cout << "1. Agregar contacto\n";
         cout << "2. Mostrar contactos\n";
         cout << "3. Ordenar por nombre\n";
-        cout << "4. Salir\n";
+        cout << "4. Buscar por nombre de manera secuencial\n";
+        cout << "5. Buscar por nombre de manera binaria\n";
+        cout << "6. Salir\n";
+        cout << "------------------------------------------\n";
         cout << "Por favor, seleccione una opcion: ";
         cin >> op;
 
         switch (op) {
         case 1:
-            agregarContacto();
+            AgregarContacto();
             break;
         case 2:
-            mostrarContactos();
+            MostrarContactos();
             break;
         case 3:
-            ordenarPorNombre();
+            OrdenarPorNombre();
             break;
         case 4:
+            cout << "Digite el nombre a buscar: ";
+            cin >> nombre;
+            secu = BusquedaSecuencialNombre(nombre);
+            if (secu != -1) {
+                cout << "El nombre del contacto encontrado es: " << arreglo[secu].nombre << ", su numero de telefono es: " << arreglo[secu].telefono << " y el correo es: " << arreglo[secu].correo << endl;
+            }
+            else {
+                cout << "\nNo se encontro el contacto...\n";
+            }
+            break;
+        case 5:
+            OrdenarPorNombre();
+            cout << "Digite el nombre a buscar: ";
+            cin >> nombre;
+            bina = BusquedaBinariaNombre(nombre);
+            if (bina != -1) {
+                cout << "El nombre del contacto encontrado es: " << arreglo[bina].nombre << ", su numero de telefono es: " << arreglo[bina].telefono << " y el correo es: " << arreglo[bina].correo << endl;
+            }
+            else {
+                cout << "\nNo se encontro el contacto...\n";
+            }
+            break;
+
+        case 6:
             cout << "\nUsted esta saliendo del programa...\n";
             break;
         }
 
-    } while (op != 4);
+    } while (op != 6);
 
     delete[] arreglo; // Liberamos memoria al final
     return 0;
